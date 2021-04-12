69d8e2857206c060e214a49ad82b3e1b1ecc5c8c
Simon El Nahas <simon.el.nahas@gmail.com>
2021-04-12 20:51:52 +0200

    project

---
+++ pages/projects/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '../../components/layout';
+import Head from 'next/head';
+
+export default function Projects() {
+  return (
+    <Layout>
+      <Head>
+        <title>Simons' Projects</title>
+      </Head>
+      <h1>hey</h1>
+    </Layout>
+  );
+}
