87e5965986b783279d579c75ba43ab9e1950bac3
Simon El Nahas <simon.el.nahas@gmail.com>
2021-06-05 10:34:55 +0200

    meta tags

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,15 +16,18 @@ export default function Layout({
     <div className={styles.container}>
       <Head>
         <link rel='icon' href='/favicon.ico' />
+        <title>Simon El Nahas</title>
+        <meta
+          name='title'
+          content='Simon El Nahas'
+        />
         <meta
           name='description'
           content='Personal website'
         />
         <meta
           property='og:image'
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={'images/pb.jpg'}
         />
         <meta
           name='og:title'
